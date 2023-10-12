e9fa6e762ed8135696cdb09048e7fb2767a1d7fc
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2023-10-12 10:20:09 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	new file:   .gitignore
    	new file:   .prettierrc.json
    	new file:   .vscode/extensions.json
    	new file:   README.md
      new file:   e2e/tsconfig.json
    	new file:   e2e/vue.spec.ts
    	new file:   env.d.ts
    	new file:   index.html
    	new file:   package.json
    	new file:   playwright.config.ts
    	new file:   public/favicon.ico
    	new file:   public/favicons/README.md
    	new file:   public/favicons/android-chrome-144x144.png
    	new file:   public/favicons/android-chrome-192x192.png
    	new file:   public/favicons/android-chrome-256x256.png
    	new file:   public/favicons/android-chrome-36x36.png
    	new file:   public/favicons/android-chrome-384x384.png
    	new file:   public/favicons/android-chrome-48x48.png
    	new file:   public/favicons/android-chrome-512x512.png
    	new file:   public/favicons/android-chrome-72x72.png
    	new file:   public/favicons/android-chrome-96x96.png
    	new file:   public/favicons/apple-touch-icon-114x114-precomposed.png
    	new file:   public/favicons/apple-touch-icon-114x114.png
    	new file:   public/favicons/apple-touch-icon-120x120-precomposed.png
    	new file:   public/favicons/apple-touch-icon-120x120.png
    	new file:   public/favicons/apple-touch-icon-144x144-precomposed.png
    	new file:   public/favicons/apple-touch-icon-144x144.png
    	new file:   public/favicons/apple-touch-icon-152x152-precomposed.png
    	new file:   public/favicons/apple-touch-icon-152x152.png
    	new file:   public/favicons/apple-touch-icon-180x180-precomposed.png
    	new file:   public/favicons/apple-touch-icon-180x180.png
    	new file:   public/favicons/apple-touch-icon-57x57-precomposed.png
    	new file:   public/favicons/apple-touch-icon-57x57.png
    	new file:   public/favicons/apple-touch-icon-60x60-precomposed.png
    	new file:   public/favicons/apple-touch-icon-60x60.png
    	new file:   public/favicons/apple-touch-icon-72x72-precomposed.png
    	new file:   public/favicons/apple-touch-icon-72x72.png
    	new file:   public/favicons/apple-touch-icon-76x76-precomposed.png
    	new file:   public/favicons/apple-touch-icon-76x76.png
    	new file:   public/favicons/apple-touch-icon-precomposed.png
    	new file:   public/favicons/apple-touch-icon.png
    	new file:   public/favicons/browserconfig.xml
    	new file:   public/favicons/favicon-16x16.png
    	new file:   public/favicons/favicon-194x194.png
    	new file:   public/favicons/favicon-32x32.png
    	new file:   public/favicons/favicon.ico
    	new file:   public/favicons/favicon.svg
    	new file:   public/favicons/html_code.html
    	new file:   public/favicons/mstile-144x144.png
    	new file:   public/favicons/mstile-150x150.png
    	new file:   public/favicons/mstile-310x150.png
    	new file:   public/favicons/mstile-310x310.png
    	new file:   public/favicons/mstile-70x70.png
    	new file:   public/favicons/safari-pinned-tab.svg
    	new file:   public/favicons/site.webmanifest
    	new file:   src/App.vue
    	new file:   src/assets/base.css
    	new file:   src/assets/blur.svg
    	new file:   src/assets/logo.svg
    	new file:   src/assets/main.css
    	new file:   src/components/HelloWorld.vue
    	new file:   src/components/HomePage.vue
    	new file:   src/components/TheWelcome.vue
    	new file:   src/components/WelcomeItem.vue
    	new file:   src/components/__tests__/HelloWorld.spec.ts
    	new file:   src/components/icons/IconCommunity.vue
    	new file:   src/components/icons/IconDocumentation.vue
    	new file:   src/components/icons/IconEcosystem.vue
    	new file:   src/components/icons/IconSupport.vue
    	new file:   src/components/icons/IconTooling.vue
    	new file:   src/main.ts
    	new file:   src/router/index.ts
    	new file:   src/stores/counter.ts
    	new file:   src/views/AboutView.vue
    	new file:   src/views/HomeView.vue
    	new file:   tsconfig.app.json
    	new file:   tsconfig.json
    	new file:   tsconfig.node.json
    	new file:   tsconfig.vitest.json
    	new file:   vite.config.ts
    	new file:   vitest.config.ts

---
+++ src/components/HomePage.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  defineProps<{
+  webAppName: string
+}>()
+</script>
+
+<template>
+  <div class="banner">
+    <img alt="HCC" class="logo" src="@/assets/logo.svg" />
+    <div class="wrapper">
+      <h1 class="blue">{{ webAppName }}</h1>
+      <h3>
+        You’ve successfully created a project with
+        <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      </h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+  }
+  
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+  
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+  }
+  
+  h3 {
+    font-size: 1.2rem;
+  }
+  
+  .banner h1,
+  .banner h3 {
+    text-align: center;
+  }
+  
+  @media (min-width: 1024px) {
+       .logo {
+    margin: 0 2rem 0 0;
+    }
+    
+    .banner h1,
+    .banner h3 {
+      text-align: left;
+    }
+  }
+</style>
